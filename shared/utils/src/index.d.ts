0a16834346565f0eea0e562607974aa664cd0f4d
Petr Sysel <petrsysel99@gmail.com>
2023-08-01 00:18:24 +0200

    Created utils library

---
+++ shared/utils/src/index.d.ts
@@ -0,0 +1,1 @@
+export { Vector2 } from "./vector2";
---
+++ shared/utils/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Vector2 = void 0;
+var vector2_1 = require("./vector2");
+Object.defineProperty(exports, "Vector2", { enumerable: true, get: function () { return vector2_1.Vector2; } });
