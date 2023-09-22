27be0064f4657ce0d889d87be7fcca0ae8c08229
Petr Sysel <petrsysel99@gmail.com>
2023-09-22 11:18:46 +0200

    Přidán mock kostýmů

---
+++ frontend/src/editor/mock/costumeDataMock.ts
@@ -0,0 +1,34 @@
+function getMockCostumes(): CostumeData[]{
+	return [
+		{
+			name: "Kozelník s klíčem",
+			path: "/frontend/costumes/Kouzelník s klíčem.png",
+			tags: ["fantasy", "kouzelník", "klíč"]
+		},
+		{
+			name: "Kozelník s plamenem",
+			path: "/frontend/costumes/Kouzelník s plamenem.png",
+			tags: ["fantasy", "kouzelník", "oheň", "plamen"]
+		},
+		{
+			name: "Kozelník",
+			path: "/frontend/costumes/Kouzelník.png",
+			tags: ["fantasy", "kouzelník"]
+		},
+		{
+			name: "Modrý klíč",
+			path: "/frontend/costumes/Modrý klíč.png",
+			tags: ["klíč"]
+		},
+		{
+			name: "Keř 1",
+			path: "/frontend/costumes/Keř 1.png",
+			tags: ["keř", "prostředí", "příroda"]
+		},
+		{
+			name: "Keř 2",
+			path: "/frontend/costumes/Keř 2.png",
+			tags: ["keř", "prostředí", "příroda"]
+		}
+	]
+}
