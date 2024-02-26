88779eea135201828ad601f42167c8781a8fcd4b
Petr Sysel <petrsysel99@gmail.com>
2024-02-26 14:58:45 +0100

    Dokončeno stylování platformy

--- frontend/src/platform/adapters/puzzleList/puzzleList.tmpl.ts
+++ frontend/src/platform/adapters/puzzleList/puzzleList.tmpl.ts
@@ -4,7 +4,9 @@ import { getQuestionMarkImg } from "../../mocks/getQuestionmarkImg";
 export const puzzleListTmpl = h('div.puzzle-list-container',
     h('div.puzzle-list-header',
         h('input.puzzle-list-search',{'placeholder':'Zadejte název úlohy'}),
-        h('button.puzzle-list-create-btn.platform-btn', "Vytvořit novou úlohu")
+        h('div.align-right',
+            h('button.puzzle-list-create-btn.platform-btn.dark-btn', "Vytvořit novou úlohu")
+        )
     ),
     h('div.puzzle-list-body-container',
         h('div.puzzle-list-empty-label', "Zatím zde nejsou žádné úlohy"),
@@ -14,11 +16,13 @@ export const puzzleListTmpl = h('div.puzzle-list-container',
                     h('img',{'src':getQuestionMarkImg()})
                 ),
                 h('div.puzzle-list-item-info',
-                    h('p.puzzle-list-item-name', "Název úlohy"),
+                    h('p.puzzle-list-item-name', "Strašně dlouhatánský název úlohy"),
                     h('p.puzzle-list-item-author', "Autor"),
                     h('p.puzzle-list-item-code', "Kód"),
                 ),
                 h('div.puzzle-list-item-action-bar',
+                    h('a.puzzle-list-item-edit',{'href':'javascript:void(0)'},
+                        h('img', {'src': './images/icons/cq-edit.png'})),
                     h('a-puzzle-list-item-play',{'href':'javascript:void(0)'},
                         h('img', {'src': './images/icons/cq-play.png'})),
                     h('a.puzzle-list-item-duplicate',{'href':'javascript:void(0)'},
@@ -28,6 +32,8 @@ export const puzzleListTmpl = h('div.puzzle-list-container',
                 )
             )
         ),
-        h('button.puzzle-list-load-more-btn.platform-btn', "Načíst další")
+        h('div.align-center',
+            h('button.puzzle-list-load-more-btn.platform-btn.dark-btn', "Načíst další")
+        )
     )
 )
