762dfffada92c757879ff283425be677e7c4e5e8
Petr Sysel <petrsysel99@gmail.com>
2024-03-25 15:01:04 +0100

    Oprava ip adresy u ServerApi

--- frontend/src/platform/ServerApi.ts
+++ frontend/src/platform/ServerApi.ts
@@ -16,8 +16,8 @@ export class ServerApi implements IServerAPI{
 	private port: number
 	private protocol: 'http' | 'https'
 	constructor(){
-		this.host = 'localhost'
-		this.port = 3000
+		this.host = '89.203.249.252'
+		this.port = 80
 		this.protocol = 'http'
 	}
 	private address(api: string){
