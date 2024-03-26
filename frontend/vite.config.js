ef97ceeeb84befaa834af164fad61c8ca38e5e45
Petr Sysel <petrsysel99@gmail.com>
2024-03-26 11:31:21 +0100

    Vite nakonfigurován pro rozdělení balíčků posle knihoven

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,14 @@ export default {
 				main: "./index.html",
 				game: "./game.html",
 				editor: "./editor.html"
-			}
+			},
+			output: {
+				manualChunks(id) {
+				  if (id.includes('node_modules')) {
+					return id.toString().split('node_modules/')[1].split('/')[0].toString();
+				  }
+				},
+			  }
 		},
 		optimizeDeps: {
 			include: ['./src/game/adapters/OBTGameLauncher/OBTGameLauncher.ts'],
